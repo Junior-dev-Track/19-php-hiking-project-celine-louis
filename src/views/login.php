e018a0b08c03e0a12af614102f48e9db36938ae5
Louis Pavier <louis.pavier@live.fr>
2024-05-24 11:37:47 +0200

    Login/register minimal front done

---
+++ src/views/login.php
@@ -0,0 +1,10 @@
+<main>
+    <form id="loginForm" action="" method="post">
+        <h2>LOGIN</h2>
+        <label for="emailNickname">Nickname or email</label>
+        <input type="text" id="emailNickname" name="emailNickname" placeholder="Nickname or email"><br>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password"><br>
+        <button class="btn" type="submit">Login</button>
+    </form>
+</main>
---
+++ src/views/register.php
@@ -0,0 +1,16 @@
+<main>
+    <form id="registerForm" action="" method="post">
+        <h2>Register</h2>
+        <label for="firstname">First Name</label>
+        <input type="text" id="firstname" name="firstname" placeholder="First Name"><br>
+        <label for="lastname">Last Name</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Last Name"><br>
+        <label for="nickname">Nickname</label>
+        <input type="text" id="nickname" name="nickname" placeholder="Nickname"><br>
+        <label for="email">Nickname</label>
+        <input type="email" id="email" name="email" placeholder="Email"><br>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password"><br>
+        <button class="btn" type="submit">Register</button>
+    </form>
+</main>
