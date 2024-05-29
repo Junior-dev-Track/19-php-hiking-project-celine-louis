7e1ebc81766e0b2f394f49125755497164536b66
Louis Pavier <louis.pavier@live.fr>
2024-05-29 16:50:59 +0200

    logout in red

--- src/views/layout.php
+++ src/views/layout.php
@@ -7,20 +7,25 @@
     <title><?= $title ?></title>
     <link rel="stylesheet" href="../style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .nav-link:hover {
+            color: #0D6EFD;
+        }
+    </style>
 </head>
 
 
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Hike</a>
+            <a class="navbar-brand">Hike</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/19-php-hiking-project-celine-louis/">Home</a>
+                        <a class="nav-link" href="/19-php-hiking-project-celine-louis/">Home</a>
                     </li>
                     <?php if (!isset($_SESSION['user']) || !$_SESSION['user']) : ?>
                         <li class="nav-item">
@@ -31,14 +36,15 @@
                         </li>
                     <?php else : ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/19-php-hiking-project-celine-louis/logout">Logout</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="/19-php-hiking-project-celine-louis/profile">User profile</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/19-php-hiking-project-celine-louis/addHike">Add Hike</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-danger border-danger" href="/19-php-hiking-project-celine-louis/logout">Logout</a>
+                        </li>
+
                     <?php endif; ?>
                 </ul>
             </div>
